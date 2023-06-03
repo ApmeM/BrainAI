6afc7b4897c0bb19783dcc1f02bee50742707448
Artem Votintsev <artem.votincev@gmail.com>
2023-06-03 23:06:19 +0200

    Add benchmarks

---
+++ BrainAI.Benchmarks/Program.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+using BrainAI.Pathfinding;
+
+[MemoryDiagnoser]
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Program>();
+    }
+
+    public enum PathfinderTypes
+    {
+        BFS,
+        Dijkstra,
+        AStar
+    }
+
+    [Params(10, 50, 100)]
+    public int ArrayLength { get; set; }
+
+    [Params(PathfinderTypes.BFS, PathfinderTypes.Dijkstra, PathfinderTypes.AStar)]
+    public PathfinderTypes PathfinderType { get; set; }
+
+    private IPathfinder<Point> pathfinder;
+
+    [IterationSetup]
+    public void Setup()
+    {
+        var graph = new AstarGridGraph(ArrayLength, ArrayLength, true);
+
+        switch (this.PathfinderType)
+        {
+            case PathfinderTypes.BFS:
+                this.pathfinder = new BreadthFirstPathfinder<Point>(graph);
+                break;
+            case PathfinderTypes.Dijkstra:
+                this.pathfinder = new WeightedPathfinder<Point>(graph);
+                break;
+            case PathfinderTypes.AStar:
+                this.pathfinder = new AStarPathfinder<Point>(graph);
+                break;
+        }
+        this.pathfinder.Search(new Point(0, 0), new Point(ArrayLength - 1, ArrayLength - 1));
+    }
+
+    [Benchmark]
+    public void Struct()
+    {
+        this.pathfinder.Search(new Point(0, 0), new Point(ArrayLength - 1, ArrayLength - 1));
+    }
+}
